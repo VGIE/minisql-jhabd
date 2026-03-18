cd22c060f8e3de2e1f6c4e89957aaac333ea4726
JonZugaza <jonzugaza23@gmail.com>
2026-03-18 12:23:03 +0100

    #18/#19 Create table regex and parse created

--- DBManager/Parser/MiniSQLParser.cs
+++ DBManager/Parser/MiniSQLParser.cs
@@ -17,7 +17,7 @@ namespace DbManager
 
             //Note: The parsing of CREATE TABLE should accept empty columns "()"`
             //And then, an execution error should be given if a CreateTable without columns is executed
-            const string createTablePattern = null;
+            const string createTablePattern = @"(?i)^CREATE\s+TABLE\s+(?<table>[a-zA-Z0-9_]+)\s*\((?<columns>[^\)]*)\)\s*;?\s*$";
 
             const string updateTablePattern = null;
 
@@ -75,6 +75,43 @@ namespace DbManager
 
                 return new Insert(table, values);
             }
+
+            
+match = Regex.Match(miniSQLQuery, createTablePattern);
+
+            if (match.Success)
+            {
+                var table = match.Groups["table"].Value;
+                var columnsText = match.Groups["columns"].Value;
+                List<string> textosColumnas = new List<string>();
+
+                if (!string.IsNullOrWhiteSpace(columnsText))
+                {
+                    textosColumnas = CommaSeparatedNames(columnsText);
+                }
+
+                List<ColumnDefinition> columns = new List<ColumnDefinition>();
+
+                foreach (string textoColumna in textosColumnas)
+                    {
+                        string[] partes = textoColumna.Split(new char[] { ' ', '\t' }, System.StringSplitOptions.RemoveEmptyEntries);
+
+                        if (partes.Length >= 2)
+                        {
+                            string nombre = partes[0]; 
+                            string tipoTexto = partes[1]; 
+
+                            ColumnDefinition.DataType tipo = System.Enum.Parse<ColumnDefinition.DataType>(tipoTexto, true);
+
+                            columns.Add(new ColumnDefinition(tipo, nombre));
+                        }
+                    }
+
+                return new CreateTable(table, columns);
+
+            }
+
+
 
             //TODO DEADLINE 4
             //Do the same for the security queries (CREATE SECURITY PROFILE, ...)
