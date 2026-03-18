d9be1470b95ac4a699b126cb9631f8654c2842f7
Hodei Garmendia Fuente <hodeigarmendia@gmail.com>
2026-03-18 12:44:40 +0100

    #16 Constructor from classes DropTable and Delete done

--- DBManager/Parser/DropTable.cs
+++ DBManager/Parser/DropTable.cs
@@ -12,6 +12,7 @@ namespace DbManager
         public DropTable(string table)
         {
             //TODO DEADLINE 2: Initialize member variables
+            this.Table = table;
             
         }
 
