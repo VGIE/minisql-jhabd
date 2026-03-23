b453006f1530618018a79dc2e8ee6120a0f03f83
JonZugaza <jonzugaza23@gmail.com>
2026-03-23 11:23:06 +0100

    #20 First CreateTable test finished (empty case)

--- OurTests/MiniSQLExecuteTests.cs
+++ OurTests/MiniSQLExecuteTests.cs
@@ -5,9 +5,16 @@ namespace OurTests
     public class MiniSQLExecuteTests
     {
         [Fact]
-        public void Test()
+        public void CreateTableVacioTest()
         {
-            Assert.True(true); 
+            String nombre = "manin";
+            List<ColumnDefinition> columns= new List<ColumnDefinition>();
+
+            CreateTable tabla = new CreateTable(nombre,columns);
+            Database testDb = new Database("db", "contraseña");
+            string result =  tabla.Execute(testDb);
+
+            Assert.Equal(Constants.DatabaseCreatedWithoutColumnsError, result);
         }
     }
 }
