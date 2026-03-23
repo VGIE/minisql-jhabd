daa5d2272574864334d778fe729cf96efcbe3513
JonZugaza <jonzugaza23@gmail.com>
2026-03-23 11:27:05 +0100

    #20 CreateTable correct test

--- OurTests/MiniSQLExecuteTests.cs
+++ OurTests/MiniSQLExecuteTests.cs
@@ -16,6 +16,21 @@ namespace OurTests
 
             Assert.Equal(Constants.DatabaseCreatedWithoutColumnsError, result);
         }
+
+        [Fact]
+        public void CreateTableTest()
+        {
+            String nombre = "manin";
+            List<ColumnDefinition> columns= new List<ColumnDefinition>();
+            columns.Add(new ColumnDefinition(ColumnDefinition.DataType.Int, "id"));
+            columns.Add(new ColumnDefinition(ColumnDefinition.DataType.String, "nombre"));
+
+            CreateTable tabla = new CreateTable(nombre,columns);
+            Database testDb = new Database("db", "contraseña");
+            string result =  tabla.Execute(testDb);
+
+            Assert.Equal(Constants.CreateTableSuccess, result);
+        }
     }
 }
 
