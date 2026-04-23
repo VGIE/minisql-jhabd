d334c7279de4525081e82b1a956e449b5a16c9af
Julensant <julensant@gmail.com>
2026-04-23 14:19:39 +0200

    #17 ManagerTests presumably fixed

--- OurTests/ManagerTests.cs
+++ OurTests/ManagerTests.cs
@@ -5,7 +5,7 @@ namespace OurTests
 {
     public class ManagerTests
     {
-        /*
+
         [Fact]
         public void IsUserAdmin()
         {
@@ -15,8 +15,8 @@ namespace OurTests
             Assert.False(new Manager("user").IsUserAdmin());
             Assert.False(new Manager(null).IsUserAdmin());
 
-            database.Save("test");
-            database = Database.Load("test", "admin", "adminPassword");
+            database.Save("IsUserAdminTest");
+            database = Database.Load("IsUserAdminTest", "admin", "adminPassword");
 
             Assert.True(database.SecurityManager.IsUserAdmin());    // Checks after reloading the database
             Assert.False(new Manager("user").IsUserAdmin());
@@ -28,28 +28,32 @@ namespace OurTests
         {
             var database = Database.CreateTestDatabase();
 
-            Assert.True(database.SecurityManager.IsGrantedPrivilege("admin", "table", Privilege.Select));   // Default checks
-            Assert.False(database.SecurityManager.IsGrantedPrivilege("user", "table", Privilege.Insert));
-            Assert.False(database.SecurityManager.IsGrantedPrivilege(null, "table", Privilege.Select));
+            Assert.True(database.SecurityManager.IsGrantedPrivilege("admin", "TestTable", Privilege.Select));   // Default checks
+            Assert.False(database.SecurityManager.IsGrantedPrivilege("user", "TestTable", Privilege.Insert));
+            Assert.False(database.SecurityManager.IsGrantedPrivilege(null, "TestTable", Privilege.Select));
+            Assert.False(database.SecurityManager.IsGrantedPrivilege("admin", null, Privilege.Select));
 
             var profile = new Profile { Name = "client" };
             var user = new User("client1", "userPassword");
             profile.Users.Add(user);
 
             database.SecurityManager.AddProfile(profile);
-            database.SecurityManager.GrantPrivilege(profile.Name, "table", Privilege.Select);
 
-            Assert.True(database.SecurityManager.IsGrantedPrivilege("client1", "table", Privilege.Select));
+            Assert.False(database.SecurityManager.IsGrantedPrivilege("client1", "TestTable", Privilege.Select));   // Check before granting the privilege
 
-            database.Save("test");
-            database = Database.Load("test", "admin", "adminPassword");
+            database.SecurityManager.GrantPrivilege(profile.Name, "TestTable", Privilege.Select);
 
-            //Assert.True(database.SecurityManager.IsGrantedPrivilege("admin", "table", Privilege.Select));   // Checks after reloading the database
-            //Assert.False(database.SecurityManager.IsGrantedPrivilege("user", "table", Privilege.Insert));
-            //Assert.False(database.SecurityManager.IsGrantedPrivilege(null, "table", Privilege.Select));
+            Assert.True(database.SecurityManager.IsGrantedPrivilege("client1", "TestTable", Privilege.Select));
 
-            //Assert.True(database.SecurityManager.IsGrantedPrivilege("client1", "table", Privilege.Select));
+            database.Save("IsGrantedPrivilegeTest");
+            database = Database.Load("IsGrantedPrivilegeTest", "admin", "adminPassword");
+
+            Assert.True(database.SecurityManager.IsGrantedPrivilege("admin", "TestTable", Privilege.Select));   // Checks after reloading the database
+            Assert.False(database.SecurityManager.IsGrantedPrivilege("user", "TestTable", Privilege.Insert));
+            Assert.False(database.SecurityManager.IsGrantedPrivilege(null, "TestTable", Privilege.Select));
+            Assert.False(database.SecurityManager.IsGrantedPrivilege("admin", null, Privilege.Select));
+
+            Assert.True(database.SecurityManager.IsGrantedPrivilege("client1", "TestTable", Privilege.Select));
         }
-        */
     }
 }
